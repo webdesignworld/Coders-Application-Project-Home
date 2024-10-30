bedbc90287f25a3a0ca798b7d1ff2707ecce17fd
Svetlana <web_design_world@outlook.com>
2024-10-30 21:19:42 +0100

    added logout to login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,7 +86,7 @@ const Navbar = () => {
                 Profile
               </Link>
               <Link
-                to="/logout"
+                to="/signin"
                 className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 Logout
